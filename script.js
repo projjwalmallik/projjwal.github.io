fb79c54ab1a0c96a7d75ad372f9bf7914e229e91
projjwalmallik <76209495+projjwalmallik@users.noreply.github.com>
2023-05-02 18:01:21 +0530

    Update script.js
    smoohtly transition

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const colors = ['#e91e63', '#2196f3', '#4caf50', '#ffc107', '#9c27b0'];
 let index = 0;
 
 function changeColor() {
-	coderBackground.style.background = `linear-gradient(-45deg, ${colors[index]} 0%, ${colors[index + 1]} 100%)`;
-	index = (index + 1) % colors.length;
+  coderBackground.style.background = `linear-gradient(-45deg, ${colors[index]} 0%, ${colors[index + 1]} 100%)`;
+  coderBackground.style.transition = 'background 1s ease-in-out';
+  index = (index + 1) % colors.length;
 }
 
 setInterval(changeColor, 1000);
